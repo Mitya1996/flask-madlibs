4b78ec85a5e751b67da5a473a5f17cd807f0deac
mits <dmitrybaycharov@gmail.com>
2021-04-01 19:13:21 -0500

    stuck on story object not being imported

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from stories import *
+from stories import story
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
 
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/story')
+def story():
+
+    print(story.prompts)
+    print(story.template)
+
+    return render_template('story.html')
